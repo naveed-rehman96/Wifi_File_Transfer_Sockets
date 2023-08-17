6a9bd4f04ff98c20809a4b37a34e2a43f345617f
naveed.rehman <naveed.rehman@aksasds.com>
2023-08-17 17:58:03 +0500

    Initial commit

---
+++ app/src/main/java/com/navdroid/WifiTransfer/Constants.kt
@@ -0,0 +1,11 @@
+package com.navdroid.WifiTransfer
+
+/**
+ * @Author: leavesCZY
+ * @Desc:
+ */
+object Constants {
+
+    const val PORT = 1995
+
+}
---
+++ app/src/main/java/com/navdroid/WifiTransfer/utils/WifiP2pUtils.kt
@@ -0,0 +1,22 @@
+package com.navdroid.WifiTransfer.utils
+
+import android.net.wifi.p2p.WifiP2pDevice
+
+/**
+ * @Author: leavesCZY
+ * @Desc:
+ */
+object WifiP2pUtils {
+
+    fun getDeviceStatus(deviceStatus: Int): String {
+        return when (deviceStatus) {
+            WifiP2pDevice.AVAILABLE -> "AVAILABLE"
+            WifiP2pDevice.INVITED -> "INVITED"
+            WifiP2pDevice.CONNECTED -> "CONNECTED"
+            WifiP2pDevice.FAILED -> "FAILED"
+            WifiP2pDevice.UNAVAILABLE -> "UNAVAILABLE"
+            else -> "unknown"
+        }
+    }
+
+}
